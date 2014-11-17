cd3c6235a606bdaedb16a0348b575f5e5c8bb011
Romik4x4 <romik-off-road@yandex.ru>
2014-11-17 13:58:12 +0300

    ok

--- _Katamaran_GPS_Barometr.ino
+++ _Katamaran_GPS_Barometr.ino
@@ -87,6 +87,7 @@ SoftwareSerial bt(23,22); // RX,TX
 #define S_COLOR     YELLOW  // second hand color
 
 int hours, minutes, seconds, ampm;
+int weekDay,monthDay,month,year;
 
 ////////////////////////////////////////////
 
@@ -341,97 +342,61 @@ void battary( void ) {
   
 }
 
-void setTime()
-{
-  
-}
 
-/*
-  displayDigitalTime() takes in values for hours, minutes, seconds
-  and am/pm. It'll print the time, in digital format, on the
-  bottom of the screen.
-*/
 void displayDigitalTime(int h, int m, int s, int ap)
 {
-  char timeChar[12];
+    char timeChar[12];
   
-  if (!ap)
-  {
-    sprintf(timeChar, "%.2d:%.2d:%.2d AM", h, m, s);
-  }
-  else
-  {
-    sprintf(timeChar, "%.2d:%.2d:%.2d PM", h, m, s);
-  }
-  /* Print the time on the clock */
-  lcd.setStr(timeChar, CLOCK_CENTER + CLOCK_RADIUS + 4, 22, 
-              C_COLOR, BACKGROUND);
+    sprintf(timeChar, "%.2d:%.2d:%.2d", h, m, s);
+    lcd.setStr(timeChar, CLOCK_CENTER + CLOCK_RADIUS + 10, 25, C_COLOR, BACKGROUND);
 }
 
-/*
-  drawClock() simply draws the outer circle of the clock, and '12',
-  '3', '6', and '9'. Room for growth here, if you want to customize
-  your clock. Maybe add dashe marks, or even all 12 digits.
-*/
 void drawClock()
 {
-  /* Draw the circle */
   lcd.setCircle(CLOCK_CENTER, 66, CLOCK_RADIUS, C_COLOR);
-  
-  /* Print 12, 3, 6, 9, a lot of arbitrary values are used here
-     for the coordinates. Just used trial and error to get them 
-     into a nice position. */
   lcd.setStr("12", CLOCK_CENTER - CLOCK_RADIUS, 66-9, C_COLOR, BACKGROUND);
   lcd.setStr("3", CLOCK_CENTER - 9, 66 + CLOCK_RADIUS - 12, C_COLOR, BACKGROUND);
   lcd.setStr("6", CLOCK_CENTER + CLOCK_RADIUS - 18, 66-4, C_COLOR, BACKGROUND);
   lcd.setStr("9", CLOCK_CENTER - 9, 66 - CLOCK_RADIUS + 4, C_COLOR, BACKGROUND);
 }
 
-/*
-  displayAnalogTime() draws the three clock hands in their proper
-  position. Room for growth here, I'd like to make the clock hands
-  arrow shaped, or at least thicker and more visible.
-*/
-void displayAnalogTime(int h, int m, int s)
-{
-  double midHours;  // this will be used to slightly adjust the hour hand
+void displayAnalogTime(int h, int m, int s) {
+  
+  double midHours;  
   static int hx, hy, mx, my, sx, sy;
   
-  /* Adjust time to shift display 90 degrees ccw
-     this will turn the clock the same direction as text */
   h -= 3;
   m -= 15;
   s -= 15;
-  if (h <= 0)
-    h += 12;
-  if (m < 0)
-    m += 60;
-  if (s < 0)
-    s += 60;
+  
+  if (h <= 0) h += 12;
+  if (m < 0)  m += 60;
+  if (s < 0)  s += 60;
     
-  /* Delete old lines: */
   lcd.setLine(CLOCK_CENTER, 66, CLOCK_CENTER+sx, 66+sy, BACKGROUND);  // delete second hand
   lcd.setLine(CLOCK_CENTER, 66, CLOCK_CENTER+mx, 66+my, BACKGROUND);  // delete minute hand
   lcd.setLine(CLOCK_CENTER, 66, CLOCK_CENTER+hx, 66+hy, BACKGROUND);  // delete hour hand
   
   /* Calculate and draw new lines: */
-  s = map(s, 0, 60, 0, 360);  // map the 0-60, to "360 degrees"
-  sx = S_LENGTH * sin(3.14 * ((double) s)/180);  // woo trig!
-  sy = S_LENGTH * cos(3.14 * ((double) s)/180);  // woo trig!
-  lcd.setLine(CLOCK_CENTER, 66, CLOCK_CENTER+sx, 66+sy, S_COLOR);  // print second hand
   
-  m = map(m, 0, 60, 0, 360);  // map the 0-60, to "360 degrees"
-  mx = M_LENGTH * sin(3.14 * ((double) m)/180);  // woo trig!
-  my = M_LENGTH * cos(3.14 * ((double) m)/180);  // woo trig!
-  lcd.setLine(CLOCK_CENTER, 66, CLOCK_CENTER+mx, 66+my, M_COLOR);  // print minute hand
+  s = map(s, 0, 60, 0, 360);                                      // map the 0-60, to "360 degrees"
+  sx = S_LENGTH * sin(3.14 * ((double) s)/180);                   // woo trig!
+  sy = S_LENGTH * cos(3.14 * ((double) s)/180);                   // woo trig!
+  lcd.setLine(CLOCK_CENTER, 66, CLOCK_CENTER+sx, 66+sy, S_COLOR); // print second hand
   
-  midHours = minutes/12;  // midHours is used to set the hours hand to middling levels between whole hours
-  h *= 5;  // Get hours and midhours to the same scale
-  h += midHours;  // add hours and midhours
-  h = map(h, 0, 60, 0, 360);  // map the 0-60, to "360 degrees"
-  hx = H_LENGTH * sin(3.14 * ((double) h)/180);  // woo trig!
-  hy = H_LENGTH * cos(3.14 * ((double) h)/180);  // woo trig!
-  lcd.setLine(CLOCK_CENTER, 66, CLOCK_CENTER+hx, 66+hy, H_COLOR);  // print hour hand
+  m = map(m, 0, 60, 0, 360);                                      // map the 0-60, to "360 degrees"
+  mx = M_LENGTH * sin(3.14 * ((double) m)/180);                   // woo trig!
+  my = M_LENGTH * cos(3.14 * ((double) m)/180);                   // woo trig!
+  lcd.setLine(CLOCK_CENTER, 66, CLOCK_CENTER+mx, 66+my, M_COLOR); // print minute hand
+  
+  midHours = minutes/12;  
+  h *= 5;                                                         
+  h += midHours;                                                  // add hours and midhours
+  h = map(h, 0, 60, 0, 360);                                      // map the 0-60, to "360 degrees"
+  hx = H_LENGTH * sin(3.14 * ((double) h)/180);                   // woo trig!
+  hy = H_LENGTH * cos(3.14 * ((double) h)/180);                   // woo trig!
+  lcd.setLine(CLOCK_CENTER, 66, CLOCK_CENTER+hx, 66+hy, H_COLOR); // print hour hand
+
 }
 
 
